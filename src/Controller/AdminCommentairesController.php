c2736035d20609d7c76b88be9cb284e423371334
fred34560 <mega-services@hotmail.fr>
2020-07-03 07:37:41 +0200

    Administration - Gestion des commentaires

---
+++ src/Controller/AdminCommentairesController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commentaires;
+use App\Form\CommentairesType;
+use App\Repository\CommentairesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/commentaires")
+ */
+class AdminCommentairesController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_commentaires_index", methods={"GET"})
+     */
+    public function index(CommentairesRepository $commentairesRepository): Response
+    {
+        return $this->render('admin/commentaires/index.html.twig', [
+            'commentaires' => $commentairesRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}/edit", name="admin_commentaires_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Commentaires $commentaire): Response
+    {
+        $form = $this->createForm(CommentairesType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaire a été modifiée avec succès !"
+            );
+
+            return $this->redirectToRoute('admin_commentaires_index');
+        }
+
+        return $this->render('admin/commentaires/edit.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="admin_commentaires_delete")
+     */
+    public function delete(Request $request, Commentaires $commentaire): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($commentaire);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'warning',
+            "Le commentaire a été supprimé avec succès !"
+        );
+
+        return $this->redirectToRoute('admin_commentaires_index');
+    }
+}
---
+++ src/Form/CommentairesType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaires;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentairesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('contenu', TextareaType::class, [
+                'label' => 'Contenu du commentaire'
+            ])
+            ->add('actif', CheckboxType::class, [
+                
+                // Pour customiser on rajoute un label_attr switch-custom ou checkbox-custom 
+                'label_attr' => ['class' => 'checkbox-custom'],
+                'label' => 'Activer le commentaire',
+                'required' => false
+                
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaires::class,
+        ]);
+    }
+}
